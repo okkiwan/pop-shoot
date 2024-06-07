450943dacd14b380989a2dd6eaf12bf9fb3937d8
okkiwan <okkiwan@tuta.io>
2024-06-07 09:07:36 +0300

    dev-script: update tmux windows indexes

--- dev.sh
+++ dev.sh
@@ -6,8 +6,8 @@ tmux new-session -d -s $SESSION
 tmux new-window -t $SESSION
 
 # send appropriate keys to each window
-tmux send-keys -t $SESSION:0 'cd .. && http-server' C-m
-tmux send-keys -t $SESSION:1 'nvim' C-m
+tmux send-keys -t $SESSION:1 'cd .. && http-server' C-m
+tmux send-keys -t $SESSION:2 'nvim' C-m
 
 # attach
 tmux a -t $SESSION
